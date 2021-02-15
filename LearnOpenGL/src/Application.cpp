1caa9456a151b18cfd18b022bc53afe25569ddfe
Emily Bender <bender.emily1@gmail.com>
2021-02-15 14:57:45 -0500

    Added GLEW to access Modern OpenGL

--- LearnOpenGL/src/Application.cpp
+++ LearnOpenGL/src/Application.cpp
@@ -1,4 +1,7 @@
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -18,6 +21,15 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+
+    /* Initialization succeeded
+    * Can use the available extensions as well as core OpenGL functionality 
+    */
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+
+    /* Print OpenGL version */ 
+    std::cout << glGetString(GL_VERSION) << std::endl; 
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
