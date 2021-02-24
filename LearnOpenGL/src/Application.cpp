085e74871c741354ef2825c988a88527edd91d05
Emily Bender <bender.emily1@gmail.com>
2021-02-24 17:36:52 -0500

    Model View Projection Matrices

--- LearnOpenGL/src/Application.cpp
+++ LearnOpenGL/src/Application.cpp
@@ -89,12 +89,25 @@ int main(void)
         * 4 by 3 aspect ratio
         * The first four values specify the bounds 
         */
-        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f); 
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        /* 
+        * view matrix 
+        * moving "camera" to the right
+        */
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        /* 
+        * moderl matrix 
+        * moving moderl 200 to the right and 200 upwards
+        */
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+        // in reverse order due to OpenGL expecting data in column major order
+        glm::mat4 mvp = proj * view * model; 
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         Texture texture("res/textures/Emily_D&P_NoBG.png");
         texture.Bind();
